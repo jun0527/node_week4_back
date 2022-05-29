2ea5964eacb66f226858cd2f73e3fcf97c893404
yijun <sandy3068@yahoo.com.tw>
2022-05-30 01:44:59 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+const errHandle = require('./handle/errHandle');
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+)
+mongoose.connect(DB)
+  .then(() => {
+    console.log('連接成功');
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+
+app.use(function(req, res, next) {
+  // res.status(404).send('Sorry cant find that!');
+  errHandle(res, 404);
+});
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  errHandle(res, 500);
+});
+
+module.exports = app;
